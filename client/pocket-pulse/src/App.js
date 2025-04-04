65e3f36636cf3bcd3b6a8e6c3cd024c878e8cd1c
Nisansala Rasanjali <IT22547774@my.sliit.lk>
2025-04-04 23:59:25 +0530

    Update App.js

--- client/pocket-pulse/src/App.js
+++ client/pocket-pulse/src/App.js
@@ -6,6 +6,7 @@ import IncomeManagement from './pages/IncomeManagement';
 import IncomeRecordsPage from './pages/IncomeRecordsPage';
 import EditIncomePage from './pages/EditIncomePage';
 import WhatIfScenarioPlanner from './pages/WhatIfScenarioPlanner';
+
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import Community from './pages/Community';
